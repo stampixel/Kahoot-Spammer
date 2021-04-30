b2afe9bf150bce855a5b4d2ad545c73d04ac0015
stampixel <76885740+stampixel@users.noreply.github.com>
2021-04-30 14:08:44 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from kahoot import client
+import random
+
+bot = client()
+
+
+
+def spambots():
+    gameid = input("Enter the game pin:")
+    botamount = input("Enter the amount of bots (max 2000):")
+    custom_username = input("Enter bots\' username:")
+
+    def joingame():
+        username = (custom_username + str(random.randint(1, 10000)))
+        bot.join((gameid), username)
+
+        def joinHandle():
+            pass
+
+        bot.on("joined", joinHandle)
+        print(f"Joined game {gameid} with username {username}.")
+        # time.sleep(.1)
+
+    for x in range(0, (int(botamount))):  # + 10  this is not code
+        joingame()
+
+
+spambots()
