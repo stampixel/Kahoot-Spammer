77297116d1735c675e34cbf0c0663658afe21272
stampixel <76885740+stampixel@users.noreply.github.com>
2021-06-15 18:27:51 -0400

    Revert "Update main.py"

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import random
 
 bot = client()
 
+
+
 def spambots():
-    gameid = input("Enter the game pin: ")
-    botamount = input("Enter the amount of bots (max 2000): ")
-    custom_username = input("Enter bots\' username: ")
+    gameid = input("Enter the game pin:")
+    botamount = input("Enter the amount of bots (max 2000):")
+    custom_username = input("Enter bots\' username:")
 
     def joingame():
         username = (custom_username + str(random.randint(1, 10000)))
@@ -16,8 +18,11 @@ def spambots():
             pass
 
         bot.on("joined", joinHandle)
-        print("Joined game"+gameid+"with username"+username+".")
+        print(f"Joined game {gameid} with username {username}.")
+        # time.sleep(.1)
 
     for x in range(0, (int(botamount))):  # + 10  this is not code
         joingame()
+
+
 spambots()
